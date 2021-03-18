f7c1977df4e36920d569cc1b11ca7eceff7a8ae5
libiqiang <biqiang.li@nongbotech.com>
2021-03-18 18:27:51 +0800

    fix

--- src/views/editor_layout.vue
+++ src/views/editor_layout.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="editor-layout">
-    
+    <div class="editor-actionpane">
+    </div>
+    <div class="editor-dockpane">
+    </div>
   </div>
 </template>
 
@@ -8,8 +11,21 @@
 // @ is an alias to /src
 export default {
   name: 'editor-layout',
-  components: {
-    
-  }
-}
+  components: {},
+};
 </script>
+<style lang="scss" scoped>
+.editor-layout {
+  height:100%;
+  width:100%;
+  position:relative;
+}
+.editor-actionpane{
+  position:absolute;
+  left:0;
+  right:0;
+  top:0;
+  background:yellowgreen;
+  height:48px;
+}
+</style>
